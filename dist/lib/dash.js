0b957992ab1b8b03de859c011c664882c94a12ab
Guntur Dwiyatmoko <guntur@solusiti.com>
2023-09-20 22:40:30 +0700

    1st init

--- dist/lib/dash.js
+++ dist/lib/dash.js
@@ -1,15 +1,15 @@
 import r from './ref.js'
 
-var modal
 
-var tasks = [
+
+var tasksExample = [
   {
     id: 'Task 1',
     name: 'Pemerintah Nusantara',
     start: '2023-03-28',
     end: '2023-12-31',
     progress: 80,
-    
+
     custom_class: 'bar-milestone' // optional
   },
 
@@ -25,7 +25,7 @@ var tasks = [
 
   {
     id: 'Task 3',
-    name: 'Habibie',
+    name: 'BJ Habibie',
     start: '2023-03-28',
     end: '2023-07-30',
     progress: 100,
@@ -59,7 +59,7 @@ var ganttOpts = {
   padding: 18,
   view_mode: 'Month',
   date_format: 'YYYY-MM-DD',
-  language: 'en', // or 'es', 'it', 'ru', 'ptBr', 'fr', 'tr', 'zh', 'de', 'hu'
+  language: "id", // or 'es', 'it', 'ru', 'ptBr', 'fr', 'tr', 'zh', 'de', 'hu'
   custom_popup_html: null
 
 }
@@ -67,76 +67,114 @@ var ganttOpts = {
 
 var g = {
 
-  modal: null,
 
-  pemdaList: () => {
+  taskList: null,
+  ganttData: null,
+  getTaskData: (cb) => {
 
-    var title = []
-    var line = [{ c: 'Daftar Pemerintah Daerah', d: { "colspan": "3", "class": "text-center font-bold" } }]
-    title.push(line)
-
-    var body = [[{ c: 'Data masih kosong', d: { "colspan": "3", "class": "text-center font-bold" } }]]
-
-
-    var foot = []
-
-    line = [{
-      d: { colspan: 3 }, c: m("p", { "class": "buttons" },
-        [
-          m("button", { "class": "btn-primary btn-sm", onclick: () => { } },
-            m("span", { "class": "icon is-small" },
-              m("i", { "class": "fa-solid fa-file-circle-plus" })
-            )
-          ),
-          m("button", { "class": "btn-warning btn-sm", onclick: () => { } },
-            m("span", { "class": "icon is-small" },
-              m("i", { "class": "fa-solid fa-folder-open" })
-            )
-          ),
-          m("button", { "class": "btn-error btn-sm" },
-            m("span", { "class": "icon is-small" },
-              m("i", { "class": "fas fa-trash" })
-            )
-          )
-        ]
-      )
+    var getTaskprm = {
+      method: "getAll",
+      tableName: "taskModel"
     }
-    ]
 
-    foot.push(line)
+    r.comm(getTaskprm, () => {
+      if (r.dataReturn.success == 1) {
+
+        g.taskList = [...r.dataReturn.message]
+
+        var Obj = { custom_class: 'bar-milestone' }
+        var allData = []
+
+        g.taskList.forEach(t => {
+          Object.assign(Obj, {
+            id: t.idTask,
+            name: t.name,
+            start: t.start,
+            end: t.end,
+            progress: t.progress,
+            dependencies: t.dependencies ? t.dependencies : null
+          })
+
+          allData.push(Obj)
+          Obj = { custom_class: 'bar-milestone' }
+        })
 
 
+        // id: 'Task 3',
+        // name: 'BJ Habibie',
+        // start: '2023-03-28',
+        // end: '2023-07-30',
+        // progress: 100,
+        // dependencies: 'Task 2',
+        // custom_class: 'bar-milestone' // optional
 
-    var comp = r.gTab("histori", { title, body, SHAK: foot })
-    g.modal = r.makeModal(comp, () => console.log('wasu'), true)
-    modal.showModal()
-    m.redraw()
+        //   {
+        //     "_id": "650a68696ccd2c972c5f4170",
+        //     "level": 1,
+        //     "idTask": "11",
+        //     "name": "Kalasan",
+        //     "start": "2023-03-01T00:00:00.000Z",
+        //     "end": "2023-04-08T00:00:00.000Z",
+        //     "progress": 20,
+        //     "created_by": "64f9cd91f33faa63db77269c",
+        //     "__v": 0
+        // }
 
-    console.log('done')
+
+        g.ganttData = allData
+
+        cb ? cb() : null
+      } else {
+        g.ganttData = tasksExample
+
+      }
+
+    })
+
+  },
+
+  showGantt: () => {
+
+    var ganttElem
+    var borokokok = (cb) => {
+      ganttElem = r.getById('gantt')
+      if (ganttElem) cb()
+    }
+    borokokok(() => {
+      var gantt = new Gantt("#gantt", g.ganttData, ganttOpts);
+      gantt.change_view_mode('Month')
+    })
+
 
 
   },
 
-  modal: r.makeModal(),
- 
+
   oninit: () => {
 
-    // if (r.logged) {
-    //   ganttOpts.view_mode = 'Month'
-    // } else m.route.set("/login")
 
+    g.getTaskData(() => {
 
+      console.log(g.ganttData, g.taskList)
+      g.showGantt()
+
+    })
 
   },
 
   oncreate: () => {
 
-    if (r.logged) {
+    // if (r.logged) {
 
-      var gantt = new Gantt("#gantt", tasks, g.opts);
-      gantt.change_view_mode('Month')
-      modal = r.getById('modalicious')
-    }
+
+
+    //     var gantt = new Gantt("#gantt", tasksExample, ganttOpts);
+    //   //  gantt.change_view_mode('Month')
+
+
+
+
+    // }
 
 
 
@@ -144,15 +182,15 @@ var g = {
 
   view: () => {
 
-    var hi 
+    var hi
     r.fullname ? hi = r.fullname.charAt(0).toUpperCase() + r.fullname.slice(1) : null
 
-    return     m('div', m('div', { class: 'text-3xl font-bold text-center mt-6' }, 'Solusi Teknologi Informasi'), 
-    m('p', { class: 'text-2xl mb-4 text-center ' }, "Manajemen Tenaga Ahli"),
-    hi == null ? m('div', { class: "text-xl text-center mb-5" }, "Silakan login") : m('div',
-      m('svg', { id: "gantt" }),
-      m('div', { id: "viewMode" }),
-      g.modal,))
+    return m('div', m('div', { class: 'text-3xl font-bold text-center mt-6' }, 'Solusi Teknologi Informasi'),
+      m('p', { class: 'text-2xl mb-4 text-center ' }, "Manajemen Tenaga Ahli"),
+      hi == null ? m('div', { class: "text-xl text-center mb-5" }, "Silakan login") : m('div',
+        m('svg', { id: "gantt" }),
+        m('div', { id: "viewMode" }),
+        g.modal,))
 
   }
 }
