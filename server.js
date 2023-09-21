d1f2465600b8ef607aaae6072e768aeaeab64a94
Guntur Dwiyatmoko <guntur@solusiti.com>
2023-09-21 22:37:54 +0700

    1st init

--- server.js
+++ server.js
@@ -11,57 +11,64 @@ const auth1 = require('./auth/authone')
 
 var nyambung = false
 
-
-const app = fastify({
-    logger: {
-        level: 'info'
-    }
-})
-
-app.register(require('@fastify/static'), {
-    root: path.join(__dirname, 'dist'),
-    prefix: '/'
-})
-
-
-app.post('/api/daftar', daftar)
-app.post('/api/login', login)
-app.post('/api/gate', gate)
-app.post('/api/auth1', auth1)
-
-
-app.get('/', (abrakadabracilukbaaaaaaa, res) => {
-
-
-    // res.send({hello:"world"})
-
-    //    res.view("/dist/wait.pug", { so: "Sally can wait"});
-
-    if (nyambung) {
-        res.sendFile("index.html");
-    } else {
-        res.sendFile("wait.html");
-    }
-
-});
-
- 
-
 connect()
     .then(smartTheo => {
         nyambung = smartTheo;
-        app.listen({ port: process.env.PORT }, (err, address) => {
-
-            if (err) {
-                app.log.error(err)
-                process.exit(1)
-            }
-            app.log.info(`server listening on ${address}`)
-        })
-
-
     })
     .catch(err => console.log(err));
+
+var init = () => {
+    const app = fastify({
+        logger: {
+            level: 'info'
+        }
+    })
+
+    app.register(require('@fastify/static'), {
+        root: path.join(__dirname, 'dist'),
+        prefix: '/'
+    })
+
+
+    app.post('/api/daftar', daftar)
+    app.post('/api/login', login)
+    app.post('/api/gate', gate)
+    app.post('/api/auth1', auth1)
+
+
+    app.get('/', (abrakadabracilukbaaaaaaa, res) => {
+
+
+
+        if (nyambung) {
+            res.sendFile("index.html");
+        } else {
+            res.sendFile("wait.html");
+        }
+
+    });
+
+    return app;
+}
+
+
+if (require.main === module) {
+    // called directly i.e. "node app"
+    init().listen({ port: process.env.PORT }, (err, address) => {
+
+        if (err) {
+            init().log.error(err)
+            process.exit(1)
+        }
+        init().log.info(`server listening on ${address}`)
+    })
+} else {
+    // required as a module => executed on aws lambda
+    module.exports = init;
+}
+
+
+
 
 
 
